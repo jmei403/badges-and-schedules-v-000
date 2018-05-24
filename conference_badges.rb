1b492c7082ab6b47b86e574f5d271f9db50d11ab
Jason Mei <jason.mei403@gmail.com>
2018-05-24 02:43:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |person|
+    badges << badge_maker(person)
+  end
+  badges
+end
+
+def assign_rooms(array)
+  array.each_with_index do |person, idx|
+    puts "Hello, #{person}! You'll be assigned to room #{idx + 1}"
+  end
+end
